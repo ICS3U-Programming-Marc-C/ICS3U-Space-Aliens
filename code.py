9b2ce430845e1ecab9a564034372f3a447df01ed
Marc Coffi <marc.coffi@stu.ocsb.ca>
2022-06-14 09:16:03 -0400

    Pushing code to github

--- code.py
+++ code.py
@@ -1,1 +1,30 @@
-print("Hello Alex!")
+#!/usr/bin/env python3
+# Created by Marc Coffi
+# Created in June 2022
+# Space Aliens Game on PyBadge
+
+import ugame
+import stage
+
+# The game_scene function is main function of the game 
+def game_scene():
+    # Defining the image banks
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+
+    # Setting the image background and pixel size
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    # Stage for the background and setting frame to 60fps
+    game = stage.Stage(ugame.display, 60)
+    
+    # Setting the layers of all sprites
+    game.layers = [background]
+
+    # Render all sprites, only the background for now
+    game.render_block()
+
+    # Repeat forever game loop
+    while True:
+        pass
+if __name__ == "__main__":
+    game_scene()
