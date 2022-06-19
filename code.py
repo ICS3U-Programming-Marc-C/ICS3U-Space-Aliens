9301d579083bb365f86887b3a2b926ffb8fed482
mc11cof <marcalistaircof@gmail.com>
2022-06-19 14:57:33 -0400

    Updating for linter

--- code.py
+++ code.py
@@ -7,19 +7,23 @@ import ugame
 import stage
 import constants
 
-# The game_scene function is main function of the game 
+# The game_scene function is main function of the game
 def game_scene():
     # Defining the image banks
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # Setting the image background and pixel size
-    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
     # Defining the space ship sprite that will be displayed every frame
-    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE))
+    ship = stage.Sprite(
+        image_bank_sprites, 5, 75, constants.SCREEN_Y - (2 * constants.SPRITE_SIZE)
+    )
     # Stage for the background and setting frame to 60fps
     game = stage.Stage(ugame.display, constants.FPS)
-    
+
     # Setting the layers of all sprites
     game.layers = [ship] + [background]
 
@@ -64,6 +68,7 @@ def game_scene():
         # Redraw Sprites
         game.render_sprites([ship])
         game.tick()
-        
+
+
 if __name__ == "__main__":
     game_scene()
